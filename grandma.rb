6b0a911695075f92bec39cd4baf1a15382486a5b
David Zeit <dzeit216@gmail.com>
2018-04-26 14:17:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,19 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!" 
-    "I LOVE YOU TOO PUMPKIN!" # don't get caught up with the order of instructions. In this case logic forces us to put the last part of the instructions first.
-  elsif phrase != phrase.upcase # Don't use a == when doing a "does not equal" operator.
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase #In this case logic dictates that if line line 18 is not true then line 20 certainly is. So we don't need an elsif statement, merely a blank else statement.
-    "NO, NOT SINCE 1938!"
+  #if phrase == "I LOVE YOU GRANDMA!" 
+   # "I LOVE YOU TOO PUMPKIN!" # don't get caught up with the order of instructions. In this case logic forces us to put the last part of the instructions first.
+  #elsif phrase != phrase.upcase # Don't use a == when doing a "does not equal" operator.
+   # "HUH?! SPEAK UP, SONNY!"
+  #elsif phrase == phrase.upcase #In this case logic dictates that if line line 18 is not true then line 20 certainly is. So we don't need an elsif statement, merely a blank else statement.
+   # "NO, NOT SINCE 1938!"
   
+  #end
+  if phrase != phrase.upcase 
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
 end 
